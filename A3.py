b774e08d265012580bb1e79cfa9e85b55ebb1114
saad09092012 <msaadatif@gmail.com>
2025-07-31 14:42:50 +0400

    A3

---
+++ A3.py
@@ -0,0 +1,8 @@
+i = int(input(" enter a number : "))
+if (i < 15):
+    print ("i is smaller then 15")
+    print ("i'm in if block")
+else:
+    print ("i is greater then 15")
+    print ("i'm in else block")
+print ("i'm not in if and not in else block")
